fc59a7e2d019102f31a736db7cd1a85b6087c0ce
andyat91 <andres.alves.91@gmail.com>
2024-04-03 11:57:14 +0200

    Create new proyect

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+
+    return(
+
+        <h5>Andrés Alves Turnes </h5>
+    )
+}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header(){
+
+
+    return(
+        <div id="header">
+        <h4>Andrés Alves Turnes </h4>
+        <ul>
+            <li>Proyectos</li>
+            <li>Skills</li>
+            <li>Recomendaciones</li>
+        </ul>
+        </div>
+    )
+}
